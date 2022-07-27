86ca2a353db1cfdeb04dd39987004760f2318854
Prajwal Nagraj <44363755+prajwal129@users.noreply.github.com>
2022-07-27 19:55:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Typed from "https://cdn.skypack.dev/typed.js@2.0.12";
 
 var typed = new Typed(".typed", {
-  strings: ["Web Developer", "data scientist", "Freelancer"],
+  strings: ["Web Developer", "data scientist", "python developer"],
   typeSpeed: 60,
   backSpeed: 60,
   loop: true
