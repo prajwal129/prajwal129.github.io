8d45535301cbb10e6073444f3d3d38ae42dc9609
Prajwal Nagraj <44363755+prajwal129@users.noreply.github.com>
2022-08-17 13:34:20 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import Typed from "https://cdn.skypack.dev/typed.js@2.0.12";
-
-var typed = new Typed(".typed", {
-  strings: ["Web Developer", "data scientist", "python developer"],
-  typeSpeed: 60,
-  backSpeed: 60,
-  loop: true
-});
